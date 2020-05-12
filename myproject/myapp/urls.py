aaaacb3ad6c395596c1ac779839d180ce3d22773
unknown <kdh4387@naver.com>
2020-05-13 07:12:21 +0900

    commit1

--- myproject/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-import myapp.views #from . import views 라고 해도 됨. 이때 6열에 있는 myapp. 도 지워줘야 함
-
-urlpatterns = [
-    path('', myapp.views.home, name = "home"),
-    path('profile/', myapp.views.profile, name = "profile")
-]
